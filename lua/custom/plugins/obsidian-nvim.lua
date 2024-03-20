58f3a91e206a71bca510160bf10ea5d6bcf67066
Rocco Gossmann <127580387+Rocco-Gossmann@users.noreply.github.com>
2024-03-20 03:46:09 +0100

    Made HTTP(S) links open in browser (on linux)

--- lua/custom/plugins/obsidian-nvim.lua
+++ lua/custom/plugins/obsidian-nvim.lua
@@ -8,13 +8,20 @@ return {
 
 	build = function()
 		os.execute("mkdir -p ~/vaults/notes")
-	end;
+	end,
 
 	opts = {
+
+		follow_url_func = function(url)
+			-- Open the URL in the default web browser.
+			vim.fn.jobstart({ "open", url }) -- Mac OS
+			-- vim.fn.jobstart({"xdg-open", url})  -- linux
+		end,
+
 		workspaces = {
 			{
-				name="Notes",
-				path="~/vaults/notes"
+				name = "Notes",
+				path = "~/vaults/notes"
 			}
 		}
 	},
